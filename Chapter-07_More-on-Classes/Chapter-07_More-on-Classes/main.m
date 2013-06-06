4356f8fc9d2f9f15edeca99289f1ea7c986a0c51
Alain Bustamante <alainbustamante@gmail.com>
2013-06-06 00:08:18 -0700

    Pre-Development Commitment
    [1]    Added workspace
    [2]    Added project folder for Chapter-07
    [3]    Added project folder for Chapter-08

---
+++ Chapter-07_More-on-Classes/Chapter-07_More-on-Classes/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Chapter-07_More-on-Classes
+//
+//  Created by Alain Bustamante on 6/6/13.
+//  Copyright (c) 2013 fonónical. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
